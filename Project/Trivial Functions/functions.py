cb7af6be0537bd34d8b4cceed1cfad600f1f0e11
vishwakftw <cs15btech11043@iith.ac.in>
2017-11-20 21:46:38 +0530

    Added Trivial Optimization test functions

---
+++ Project/Trivial Functions/functions.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+# Index of references
+# B1 -> Bohachevsky Function 1
+# B2 -> Bohachevsky Function 2
+# B3 -> Bohachevsky Function 3
+# RB -> Rosenbrock Function
+# BL -> Beale's Function
+# ST -> Styblinsky-Tang's Function
+
+def B1(X):
+    """
+        Returns Bohachevsky function 1 at a point
+    """
+    x = X[0]
+    y = X[1]
+    return x**2 + 2.0*(y**2) - 0.3*np.cos(3.0*np.pi*x) - 0.4*np.cos(4.0*np.pi*y) + 0.7
+
+def B2(X):
+    """
+        Returns Bohachevsky function 2 at a point
+    """
+    x = X[0]
+    y = X[1]
+    return x**2 + 2.0*(y**2) - 0.3*np.cos(3.0*np.pi*x)*np.cos(4.0*np.pi*y) + 0.3
+
+def B3(X):
+    """
+        Returns Bohachevsky function 3 at a point
+    """
+    x = X[0]
+    y = X[1]
+    return x**2 + 2.0*(y**2) - 0.3*np.cos(3.0*np.pi*x + 4.0*np.pi*y) + 0.3
+
+def RB(X):
+    """
+        Returns Rosenbrock's function at a point
+    """
+    x = X[0]
+    y = X[1]
+    return 25*(y - x**2)**2 + (1 - x)**2
+
+def BL(X):
+    """
+        Returns Beale's function at a point
+    """
+    x = X[0]
+    y = X[1]
+    term1 = (1.5 - x + x*y)**2
+    term2 = (2.25 - x + x*y**2)**2
+    term3 = (2.625 - x + x*y**3)**2
+    return term1 + term2 + term3
+
+def ST(X):
+    """
+        Returns Styblinsky-Tang's function at a point
+    """
+    x = X[0]
+    y = X[1]
+    return x*4 - 16*(x**2) + 5*x + y**4 - 16*(y**2) + 5*y
---
+++ Project/Trivial Functions/gradients.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+# Index of references
+# B1 -> Bohachevsky Function 1
+# B2 -> Bohachevsky Function 2
+# B3 -> Bohachevsky Function 3
+# RB -> Rosenbrock Function
+# BL -> Beale's Function
+# ST -> Styblinsky-Tang's Function
+
+def B0(X):
+    """
+        Returns gradient of Bohachevsky Function 1 at a point
+    """
+    x = X[0]
+    y = X[1]
+    dx = 2*x + 3.0*np.pi*0.3*np.sin(3.0*np.pi*x)
+    dy = 4*y + 4.0*np.pi*0.4*np.sin(4.0*np.pi*y)
+    return np.array([dx, dy])
+
+def B1(X):
+    """
+        Returns gradient of Bohachevsky Function 2 at a point
+    """
+    x = X[0]
+    y = X[1]
+    dx = 2*x + 3.0*np.pi*0.3*np.sin(3.0*np.pi*x)*np.cos(4.0*np.pi*y)
+    dy = 4*y + 4.0*np.pi*0.4*np.sin(4.0*np.pi*y)*np.cos(3.0*np.pi*x)
+    return np.array([dx, dy])
+
+def B2(X):
+    """
+        Returns gradient of Bohachevsky Function 3 at a point
+    """
+    x = X[0]
+    y = X[1]
+    dx = 2*x + 3.0*np.pi*0.3*np.sin(3.0*np.pi*x + 4.0*np.pi*y)
+    dy = 4*y + 4.0*np.pi*0.4*np.sin(3.0*np.pi*x + 4.0*np.pi*y)
+    return np.array([dx, dy])
+
+def RB(X):
+    """
+        Returns gradient of Rosenbrock's function at a point
+    """
+    x = X[0]
+    y = X[1]
+    dx = -100*(y - x**2)*x + 2*(x - 1)
+    dy = 50*(y - x**2)
+    return np.array([dx, dy])
+
+def BL(X):
+    """
+        Returns gradient of Beale's function at a point
+    """
+    x = X[0]
+    y = X[1]
+	cmn1	= 2*(1.5 - x + x*y)
+	cmn2	= 2*(2.25 - x + x*y**2)
+	cmn3	= 2*(2.625 - x + x*y**3)
+	tmx1	= -1 + y
+	tmx2	= -1 + y**2
+	tmx3	= -1 + y**3
+	tmy1	= x
+	tmy2	= 2*x*y
+	tmy3	= 3*x*y**2
+	dx	= cmn1*tmx1 + cmn2*tmx2 + cmn3*tmx3
+	dy	= cmn1*tmy1 + cmn2*tmy2 + cmn3*tmy3
+	return np.array([dx, dy])
+
+def ST(X):
+    """
+        Returns gradient of Styblinsky-Tang's function at a point
+    """
+    x = X[0]
+    y = X[1]
+    dx = 4*x**3 - 32*x + 5
+    dy = 4*y**3 - 32*y + 5
