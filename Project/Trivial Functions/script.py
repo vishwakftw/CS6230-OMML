54d44569d1e54b09090c02eea334e3eb69fa7edd
vishwakftw <cs15btech11043@iith.ac.in>
2017-12-05 00:10:03 +0530

    get best params for trivial functions

--- Project/Trivial Functions/script.py
+++ Project/Trivial Functions/script.py
@@ -9,6 +9,7 @@ for i in functions:
     for j in optims:
         subprocess.call(["python3", "main.py", "--optim", j, "--fn", i]) 
 
+best_files = open('best_values.txt', 'w')
 for directory_1 in functions:
     for directory_2 in optims:
         data = []
@@ -16,4 +17,6 @@ for directory_1 in functions:
         for filename in file_list:
             filepath = directory_1 +'/'+ directory_2 +'/'+ filename
             data.append(np.genfromtxt(filepath)[-1,1])
-        print(directory_1, directory_2, file_list[np.argsort(data)[-1]])
+        print(directory_1, directory_2, file_list[np.argsort(data)[0]])
+        best_files.write('{0}\t{1}\t{2}\n'.format(directory_1, directory_2, file_list[np.argsort(data)[0]][3:-7]))
+best_files.close()
